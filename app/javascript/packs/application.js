6684b3503e8b4a68dffcc10ed65cf2dca07bb175
yoshihiro201104 <187289075+yoshihiro201104@users.noreply.github.com>
2025-02-19 04:32:30 +0000

    bootstarp

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,18 +1,15 @@
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
+// app/javascript/packs/application.js
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
+import Rails from "@rails/ujs";
+import Turbolinks from "turbolinks";
+import * as ActiveStorage from "@rails/activestorage";
+import "channels";
 
 import "jquery";
 import "popper.js";
-import "bootstrap";
-import "../stylesheets/application"; 
+import "bootstrap"; // BootstrapのJavaScriptを読み込み
+import "../stylesheets/application"; // SCSSファイルを読み込み
 
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
+Rails.start();
+Turbolinks.start();
+ActiveStorage.start();
