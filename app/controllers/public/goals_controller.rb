48e5526fcecd3b45a1ceaf88d8883203e18f8516
yoshihiro201104 <187289075+yoshihiro201104@users.noreply.github.com>
2025-03-03 12:05:52 +0000

    画像認識

--- app/controllers/public/goals_controller.rb
+++ app/controllers/public/goals_controller.rb
@@ -9,8 +9,13 @@ class Public::GoalsController < ApplicationController
   def create
     # 現在のログインユーザーのフォームにパラメータを入れる、または、入った状態
     @goal = current_user.goals.new(goal_params)
+    # 投稿した画像をAIに渡す
+    tags = Vision.get_image_data(goal_params[:image])
     # データをデータベースに保存するためのsaveメソッド実行
     if @goal.save
+      tags.each do |tag|
+        @goal.tags.create(name: tag)
+      end
       # トップ画面へリダイレクト
       redirect_to goal_path(@goal), notice: "目標が登録されました！"
     else
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2025_02_20_124920) do
+ActiveRecord::Schema.define(version: 2025_03_03_113650) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -101,6 +101,14 @@ ActiveRecord::Schema.define(version: 2025_02_20_124920) do
     t.index ["user_id"], name: "index_permits_on_user_id"
   end
 
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
+    t.integer "goal_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["goal_id"], name: "index_tags_on_goal_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -122,4 +130,5 @@ ActiveRecord::Schema.define(version: 2025_02_20_124920) do
   add_foreign_key "group_users", "users"
   add_foreign_key "permits", "groups"
   add_foreign_key "permits", "users"
+  add_foreign_key "tags", "goals"
 end
