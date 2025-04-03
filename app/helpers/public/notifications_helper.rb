bfa94f3b2a7ffba391aad0d979afdd7f8d710f8a
yoshihiro201104 <187289075+yoshihiro201104@users.noreply.github.com>
2025-04-03 08:32:29 +0000

    通知機能修正

--- app/helpers/public/notifications_helper.rb
+++ app/helpers/public/notifications_helper.rb
@@ -2,7 +2,7 @@ module Public::NotificationsHelper
   def notification_message(notification)
     case notification.notifiable_type
     when "Goal"
-      "フォローしている#{notification.notifiable.user.name}さんが#{notification.notifiable.goal}を投稿しました"
+      "フォローしている#{notification.notifiable.user.name}さんが「#{notification.notifiable.goal}」を投稿しました"
     else
       "投稿した#{notification.notifiable.goal.goal}が#{notification.notifiable.user.name}さんにいいねされました"
     end
